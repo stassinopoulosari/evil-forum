f99d0b821d33239a0eb9c45bca172f8195ee9466
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2024-11-05 11:35:39 -0800

    Move entry points and add favicons

--- db/users.js
+++ db/users.js
@@ -1,7 +1,10 @@
+import { HOMEPAGE_ITEMS_PER_PAGE } from "../config.js";
 import {
   client,
   paramArgumentNonNull,
+  paramArgumentNumber,
   paramArgumentString,
+  paramArgumentStringNotBlank,
   validateArgument,
 } from "./db.js";
 import {
@@ -107,6 +110,77 @@ export const dbUserActive = async (userID) => {
         return undefined;
       }
       return userQuery.rows[0];
+    } catch (err) {
+      throw POSTGRES_ERROR(err);
+    }
+  },
+  dbGetUserContent = async (username, requestorUserID, page) => {
+    if (client === undefined) throw NO_CLIENT_ERROR;
+    validateArgument("username", username, [
+      paramArgumentNonNull,
+      paramArgumentStringNotBlank,
+    ]);
+    validateArgument("requestorUserID", requestorUserID, [paramArgumentString]);
+    validateArgument("page", page, [paramArgumentNumber]);
+    let offset;
+    if (page === undefined) offset = 0;
+    offset = HOMEPAGE_ITEMS_PER_PAGE * Math.floor(page ?? 0);
+    try {
+      return (
+        await client.query(
+          `select
+        coalesce(comments.post_id, posts.post_id) as post_id,
+        post_title,
+        post_timestamp,
+        post_text,
+        post_link,
+        post_votes,
+        post_score,
+        post_locked,
+        post_edited_at,
+        user_displayname,
+        comment_id,
+        comment_replyto,
+        comment_root,
+        comment_votes,
+        comment_content,
+        comment_timestamp,
+        comment_locked,
+        coalesce(post_timestamp, comment_timestamp) as timestamp,
+        case
+          when posts.post_id is not null then 'post'
+          when comments.comment_id is not null then 'comment'
+        else
+          NULL
+        end as entity_type,
+        case
+          when posts.post_id is not null and posts.user_id = $2 then TRUE
+          when posts.post_id is null then NULL
+          else
+            false
+        end as post_mine,
+        case
+          when comments.comment_id is not null and comments.user_id = $2 then TRUE
+          when comments.comment_id is null then NULL
+          else
+            false
+        end as comment_mine
+      from
+        posts
+        full outer join
+          comments on FALSE
+        left join
+          users on
+            users.user_id = coalesce(posts.user_id, comments.user_id)
+        where
+          user_username = $1
+        order by
+          timestamp desc
+        limit $3
+        offset $4;`,
+          [username, requestorUserID, HOMEPAGE_ITEMS_PER_PAGE, offset],
+        )
+      ).rows;
     } catch (err) {
       throw POSTGRES_ERROR(err);
     }
